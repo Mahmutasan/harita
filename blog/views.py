0a35768c1bf3efb7ff99b63e27d8892bf6ae3f30
Mahmutasan <ceremy16@gmail.com>
2017-03-17 14:46:15 +0200

    bosluk duzeltildi

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
-     return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {})
